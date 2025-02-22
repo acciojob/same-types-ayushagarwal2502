d99614968f1eb6345e270ea50be2ce3265e4d10e
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-02-22 23:56:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function isSameType(value1, value2) {
    if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
- else if(typeof(value1) === typeof(value2)) {
+ else if(typeof(value1) == typeof(value2)) {
 	 
     return true;
   }
