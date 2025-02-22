08639cf53fcc50937854589d32069867ed5e4ec8
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-02-23 00:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function isSameType(value1, value2) {
+   // Check if the values are NaN
    if (Number.isNaN(value1) && Number.isNaN(value2)) {
-    return true;
-  }
- else if(typeof(value1) == typeof(value2)) {
-	 
-    return true;
-  }
-	else{
-  return false;
-	}
+     return true;
+   }
+   
+   // Check if the values are numbers and convert them to number type
+   if (!isNaN(value1)) {
+     value1 = Number(value1);
+   }
+   if (!isNaN(value2)) {
+     value2 = Number(value2);
+   }
+   
+   // Compare the types of the values
+   if (typeof(value1) == typeof(value2)) {
+     return true;
+   }
+   else {
+     return false;
+   }
 }
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
